a0dba97d6b3ae0c0623ec92185eff11ae1e8385b
CyberSecurity0110 <galaxyman0110@gmail.com>
2022-02-15 14:40:15 +0330

    counter-app

--- myapp/views.py
+++ myapp/views.py
@@ -21,6 +21,16 @@ from django.shortcuts import render
 #         request.session['count'] = 0
 #     return render(request, 'counter.html')
 
+# def counter(request):
+#     if request.method == "POST" and 'count' in request.POST:
+#         try:
+#             request.session['count'] += 1
+#         except:
+#             request.session['count'] = 0
+#     elif request.method == 'POST' and 'reset' in request.POST:
+#         request.session['count'] = 0
+#     return render(request, 'counter.html')
+
 def counter(request):
     if request.method == "POST" and 'count' in request.POST:
         try:
